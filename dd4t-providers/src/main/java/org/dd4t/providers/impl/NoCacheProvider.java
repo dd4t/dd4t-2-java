09beec22ea183071f2583a99ca3297a95ef616a0
Rogier Oudshoorn <rogier.oudshoorn@gmail.com>
2015-02-13 15:49:16 +0100

    Adding nocacheprovider for testing purposes

--- dd4t-providers/src/main/java/org/dd4t/providers/impl/NoCacheProvider.java
+++ dd4t-providers/src/main/java/org/dd4t/providers/impl/NoCacheProvider.java
@@ -1,8 +1,12 @@
 package org.dd4t.providers.impl;
 
+import java.util.Collection;
+
+import org.dd4t.core.caching.Cachable;
 import org.dd4t.core.caching.CacheElement;
 import org.dd4t.core.caching.impl.CacheElementImpl;
 import org.dd4t.providers.CacheProvider;
+import org.dd4t.providers.PayloadCacheProvider;
 
 /**
  * CacheProvider which doesn't cache anything.
@@ -10,7 +14,7 @@ import org.dd4t.providers.CacheProvider;
  * @author rooudsho
  *
  */
-public class NoCacheProvider implements CacheProvider {
+public class NoCacheProvider implements PayloadCacheProvider, CacheProvider {
 
 	@Override
 	public <T> void storeInItemCache(String key, CacheElement<T> cacheElement) {
@@ -24,8 +28,32 @@ public class NoCacheProvider implements CacheProvider {
 	}
 
 	@Override
-	public <T> CacheElement<T> loadFromLocalCache(String key) {
+	public <T> CacheElement<T> loadPayloadFromLocalCache(String key) {
 		return new CacheElementImpl<T>(null, true);
 	}
 
+	@Override
+	public void storeInCache(String key, Cachable ob, Collection<Cachable> deps) {		
+	}
+
+	@Override
+	public void storeInItemCache(String key, Object ob,
+			int dependingPublicationId, int dependingItemId) {		
+	}
+
+	@Override
+	public void storeInComponentPresentationCache(String key, Object ob,
+			int dependingPublicationId, int dependingCompId,
+			int dependingTemplateId) {		
+	}
+
+	@Override
+	public void storeInKeywordCache(String key, Object ob,
+			int dependingPublicationId, int dependingItemId) {		
+	}
+
+	@Override
+	public Object loadFromLocalCache(String key) {
+		return null;
+	}
 }

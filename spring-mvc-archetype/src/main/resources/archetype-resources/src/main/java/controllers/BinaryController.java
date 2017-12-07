ff701b10985ce5f50110628520304cd198c2f7cb
Raimond Kempees <kempees@radagio.com>
2017-12-07 13:52:10 +0100

    Formatting

--- spring-mvc-archetype/src/main/resources/archetype-resources/src/main/java/controllers/BinaryController.java
+++ spring-mvc-archetype/src/main/resources/archetype-resources/src/main/java/controllers/BinaryController.java
@@ -1,6 +1,6 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
+#set($symbol_pound='#')
+        #set($symbol_dollar='$')
+        #set($symbol_escape='\' )
 /*
  * Copyright (c) 2015 SDL, Radagio & R. Oudshoorn
  *
@@ -17,58 +17,66 @@
  * limitations under the License.
  */
 
-package ${package}.controllers;
+        package ${package}.controllers;
 
-import org.dd4t.core.exceptions.ItemNotFoundException;
-import org.dd4t.mvc.controllers.AbstractBinaryController;
+        import org.dd4t.core.exceptions.ItemNotFoundException;
+        import org.dd4t.mvc.controllers.AbstractBinaryController;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
 
 /**
  * dd4t-2
  *
  * @author R. Kempees
  */
-@RequestMapping (value = {"**/*.gif", "**/*.jpg", "**/*.jpeg", "**/*.png", "**/*.pdf", "**/*.vcf", "**/*.swf", "**/*.zip", "**/*.xls", "**/*.xlsx"})
+@RequestMapping (value = {"**/*.gif", "**/*.jpg", "**/*.jpeg", "**/*.png", "**/*.pdf", "**/*.vcf", "**/*.swf", "**/*" +
+        ".zip", "**/*.xls", "**/*.xlsx"})
 public class BinaryController extends AbstractBinaryController {
-	@Override
-	@RequestMapping(method = {RequestMethod.GET, RequestMethod.HEAD})
-	public void getBinary (final HttpServletRequest request, final HttpServletResponse response) throws ItemNotFoundException {
-		super.getBinary(request, response);
-	}
+    @Override
+    @RequestMapping (method = {RequestMethod.GET, RequestMethod.HEAD})
+    public void getBinary(final HttpServletRequest request, final HttpServletResponse response) throws
+            ItemNotFoundException {
+        super.getBinary(request, response);
+    }
 
-	@Override protected String getBinaryPath (final HttpServletRequest request) {
-		return super.getBinaryPath(request);
-	}
+    @Override
+    protected String getBinaryPath(final HttpServletRequest request) {
+        return super.getBinaryPath(request);
+    }
 
-	@Override public void setBinaryRootFolder (final String binaryRootFolder) {
-		super.setBinaryRootFolder(binaryRootFolder);
-	}
+    @Override
+    public void setBinaryRootFolder(final String binaryRootFolder) {
+        super.setBinaryRootFolder(binaryRootFolder);
+    }
 
-	@Override public boolean isUseBinaryStorage () {
-		return super.isUseBinaryStorage();
-	}
+    @Override
+    public boolean isUseBinaryStorage() {
+        return super.isUseBinaryStorage();
+    }
 
-	@Override public void setUseBinaryStorage (final boolean useBinaryStorage) {
-		super.setUseBinaryStorage(useBinaryStorage);
-	}
+    @Override
+    public void setUseBinaryStorage(final boolean useBinaryStorage) {
+        super.setUseBinaryStorage(useBinaryStorage);
+    }
 
-	/**
-	 * @return the removeContextPath
-	 */
-	@Override public boolean isRemoveContextPath () {
-		return super.isRemoveContextPath();
-	}
+    /**
+     * @return the removeContextPath
+     */
+    @Override
+    public boolean isRemoveContextPath() {
+        return super.isRemoveContextPath();
+    }
 
-	/**
-	 * @param removeContextPath the removeContextPath to set
-	 */
-	@Override public void setRemoveContextPath (final boolean removeContextPath) {
-		super.setRemoveContextPath(removeContextPath);
-	}
+    /**
+     * @param removeContextPath the removeContextPath to set
+     */
+    @Override
+    public void setRemoveContextPath(final boolean removeContextPath) {
+        super.setRemoveContextPath(removeContextPath);
+    }
 }
